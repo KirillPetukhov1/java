769c0cfc53d8b26e3be3c938062928cfc9cf9d9d
Kirill <kpetuhov3242@gmail.com>
2025-09-18 21:12:15 +0300

    lab_1 finish

--- lab_1/src/Main.java
+++ lab_1/src/Main.java
@@ -1,6 +1,51 @@
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
-        
+        int[] s = new int[19];
+        float[] x = new float[12];
+        double[][] s1 = new double[10][12];
+
+        for (int i = 0; i < 19; i++) {
+            s[i] = 23 - i;
+        }
+
+        for (int i = 0; i < 12; i++) {
+            x[i] = getRandomInRange(-14.0F, 13.0F);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 12; j++) {
+                s1[i][j] = calcNewElement(s[i], x[j]);
+            }
+        }
+
+        showMatrix(s1);
+    }
+
+
+    public static float getRandomInRange(float min, float max) {
+        return (float) (Math.random() * ((max - min) + 1)) + min;
+    }
+
+
+    public static double calcNewElement(int s, float x) {
+        if (s == 17) {
+            return Math.asin(Math.cos(Math.atan(Math.pow((x - 0.5) / 27, 2))));
+        } else if (s == 5 || s == 7 || s == 9 || s == 21 || s == 23) {
+            return 2 / (0.25 - Math.pow(Math.cos(x), 3));
+        } else {
+            return Math.sin(Math.sin(Math.pow(Math.cbrt(x) / (0.5 + Math.cos(x)), 2)));
+        }
+    }
+
+
+    public static void showMatrix(double[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.printf("%7.3f ", matrix[i][j]);
+            }
+            System.out.print('\n');
+        }
     }
 }
